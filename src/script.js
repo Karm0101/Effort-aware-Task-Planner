d746b493efbc6fd8990141e33db1221daaef8f86
Karm0101 <karmtawfiq@gmail.com>
2025-06-26 21:16:09 +0100

    Fixed adding new tasks and styled them

--- src/script.js
+++ src/script.js
@@ -2,14 +2,16 @@ const addTaskButton = document.getElementById('addTaskButton');
 
 function addTask() {
     const newDiv = document.createElement("div");
-    document.body.appendChild(newDiv)
     const taskBox = document.createElement("input");
     const deadlineBox = document.createElement("input");
 
     newDiv.appendChild(taskBox);
     newDiv.appendChild(deadlineBox);
 
-    console.log("hi");
+    taskBox.classList.add("taskClass")
+    deadlineBox.classList.add("deadlineClass")
+
+    document.getElementById('taskContainer').appendChild(newDiv);
 }
 
 addTaskButton.addEventListener("click", addTask);
