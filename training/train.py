bd3e1155dad6270e57e3192833bec43233187517
Karm0101 <karmtawfiq@gmail.com>
2025-07-16 21:29:53 +0100

    Saved a trained instance of the model as a pickle file

--- training/train.py
+++ training/train.py
@@ -1,6 +1,7 @@
 from preprocess import preprocessor
 from naive_bayes import NaiveBayesClassifier
 import json
+import pickle
 
 def training():
     with open('training\dataset.json', 'r') as f:
@@ -10,3 +11,10 @@ def training():
         dataset[i]['task'] = preprocessor(dataset[i]['task'])
 
     model = NaiveBayesClassifier()
+    model.train(dataset)
+
+    with open('model.pickle', 'wb') as file:
+        pickle.dump(model, file)
+
+if __name__ == '__main__':
+    training()
