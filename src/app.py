37b634bdb0ff27620fd2930609aa629210162ab6
Karm0101 <karmtawfiq@gmail.com>
2025-07-20 11:17:22 +0100

    Created a function in the backend which takes in the task descriptions and deadlines from the frontend, predicts the easiness, importance  and priority for each task and then returns the priority for each task

--- src/app.py
+++ src/app.py
@@ -1,4 +1,60 @@
 from flask import Flask, render_template
+import pickle
+
+def effortPredictions(tasks):
+    with open('effortModel.pickle', 'rb') as file:
+        effortModel = pickle.load(file)
+    
+    effortPreds = effortModel.prediction(tasks)
+    return effortPreds
+
+def importancePredictions(tasks):
+    with open('importanceModel.pickle', 'rb') as file:
+        importanceModel = pickle.load(file)
+    
+    importancePreds = importanceModel.prediction(tasks)
+    return importancePreds
+
+def easinessScore(effortPrediction):
+    if effortPrediction == 'very easy':
+        easiness = 4
+    if effortPrediction == 'easy':
+        easiness = 3
+    if effortPrediction == 'moderate':
+        easiness = 2
+    else:
+        easiness = 1
+    
+    return easiness
+
+def importanceScore(importancePrediction):
+    if importancePrediction == 'high importance':
+        importance = 4
+    if importancePrediction == 'medium importance':
+        importance = 3
+    if importancePrediction == 'low importance':
+        importance = 2
+    else:
+        importance = 1
+    
+    return importance
+
+def sortTasks(tasks, deadlines):
+    effortPreds = effortPredictions(tasks)
+    importancePreds = importancePredictions(tasks)
+    tasksPriority = {}
+    
+    for i in range(len(tasks)):
+        easiness = easinessScore(effortPreds[i])
+        importance = importanceScore(importancePreds[i])
+        urgency = 1/(deadlines[i] + 1)
+
+        priority = easiness * importance * urgency
+
+        tasksPriority.update({tasks[i]: priority})
+    
+    return tasksPriority
+
 app = Flask(__name__)
 
 @app.route("/")
